46a7a6f95fa76ba64dac8824a2cb984dda81bfd0
jebster <jensentan88@gmail.com>
2013-04-05 16:53:05 +0800

    More HOD elements

--- js/input.js
+++ js/input.js
@@ -8,3 +8,31 @@ function displayAbstractTwo(){
 	document.getElementById('abstractOne').style.zIndex = 4;
 	document.getElementById('abstractTwo').style.zIndex = 5;
 }
+
+function viewCalendar(){
+	document.getElementById('calendar-modal').style.left = "-5px";
+	document.getElementById('date-list-modal').style.left = "-1000px";
+	document.getElementById('competitor-list-modal').style.left = "-1000px";
+}
+
+function viewCompetitors(){
+	document.getElementById('calendar-modal').style.left = "-1000px";
+	document.getElementById('date-list-modal').style.left = "-1000px";
+	document.getElementById('competitor-list-modal').style.left = "-5px";	
+}
+
+function viewDateList(){
+	document.getElementById('calendar-modal').style.left = "-1000px";
+	document.getElementById('date-list-modal').style.left = "-5px";
+	document.getElementById('competitor-list-modal').style.left = "-1000px";
+}
+
+function closeModal(){
+	document.getElementById('calendar-modal').style.left = "-1000px";
+	document.getElementById('date-list-modal').style.left = "-1000px";
+	document.getElementById('competitor-list-modal').style.left = "-1000px";
+}
+
+
+
+
