15755ed6bec0b5993599289e154d1b456568ddba
jebster <jensentan88@gmail.com>
2013-04-05 13:56:06 +0800

    General Layout
    Not finalized yet. Will put in more things later.

---
+++ js/input.js
@@ -0,0 +1,10 @@
+
+function displayAbstractOne(){
+	document.getElementById('abstractOne').style.zIndex = 5;
+	document.getElementById('abstractTwo').style.zIndex = 4;
+}
+
+function displayAbstractTwo(){
+	document.getElementById('abstractOne').style.zIndex = 4;
+	document.getElementById('abstractTwo').style.zIndex = 5;
+}
